cb3e03753fa282c41d2f9ee75bede4e025ddf17d
Gary <1572261708@qq.com>
2024-12-01 02:05:22 +0800

    update功能还是不能用

--- service/user/api/internal/logic/updateadminlogic.go
+++ service/user/api/internal/logic/updateadminlogic.go
@@ -2,10 +2,9 @@ package logic
 
 import (
 	"context"
-	"mall/common/errorx"
+
 	"mall/service/user/api/internal/svc"
 	"mall/service/user/api/internal/types"
-	"mall/service/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -25,19 +24,7 @@ func NewUpdateAdminLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Updat
 }
 
 func (l *UpdateAdminLogic) UpdateAdmin(req *types.UpdateAdminRequest) (resp *types.UpdateAdminResponse, err error) {
-	// 调用 RPC 服务更新管理员
-	res, err := l.svcCtx.UserRpc.UpdateAdmin(l.ctx, &user.UpdateAdminRequest{
-		Id:       req.Id,
-		Password: req.Password,
-		Level:    req.Level,
-	})
-	if err != nil {
-		return nil, errorx.NewDefaultError(err.Error())
-	}
+	// todo: add your logic here and delete this line
 
-	return &types.UpdateAdminResponse{
-		Id:       res.Id,
-		Username: res.Username,
-		Level:    res.Level,
-	}, nil
+	return
 }
